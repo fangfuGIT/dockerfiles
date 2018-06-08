507a1508e91000c6e262266d3bf93fb967a29271
fangfuGIT <39209557@qq.com>
2018-06-08 18:07:00 +0800

    0608a

---
+++ .bash_history
@@ -0,0 +1,23 @@
+cd root
+ll
+ls
+more test2
+more test2.txt
+vi test2.txt
+ls
+more test
+cd test/
+ll
+cd ..
+ls
+touch mytest.txt
+ls
+exit
+cd
+more mytest.txt 
+eixt
+eixt
+eixt
+eixt
+eixt
+exit
---
+++ flask-redis/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from redis import Redis
+import os
+import socket
+
+app = Flask(__name__)
+redis = Redis(host=os.environ.get('REDIS_HOST', '127.0.0.1'), port=6379)
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return 'Hello Container World, I have been seen '
